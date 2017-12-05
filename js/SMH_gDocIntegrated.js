4a54f1c970dd6a49e03f5821bea94912e507b713
Jason MacDonald <jsmacdonald@gmail.com>
2017-12-05 09:41:17 -0800

    New Online Draft Started
    Had to change what spreadsheet and range that the ApiCall was referring
    to.

--- js/SMH_gDocIntegrated.js
+++ js/SMH_gDocIntegrated.js
@@ -1,9 +1,9 @@
 function makeApiCall() {
   var params = {
     // The ID of the spreadsheet to retrieve data from.
-    spreadsheetId: '1tXi7pCJrgOadsq5DqmQa6abYy4f-IXTii-CfXGr2NU4',  // TODO: Update placeholder value.
+    spreadsheetId: '1R1yMOf_rem3MJgueJg7ADEDu7lLjtlcXFBPU2lEbOb4',  // TODO: Update placeholder value.
     // The A1 notation of the values to retrieve.
-    range: 'B4:O39',  // TODO: Update placeholder value.
+    range: 'B4:I43',  // TODO: Update placeholder value.
     // How values should be represented in the output.
     // The default render option is ValueRenderOption.FORMATTED_VALUE.
     // valueRenderOption: '',  // TODO: Update placeholder value.
