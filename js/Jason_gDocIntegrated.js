887f9a203584e258c85218a59113fe3c5da5ae4a
Jason MacDonald <jsmacdonald@gmail.com>
2018-04-03 15:26:00 -0700

    Additions and Changes
    In:
    Mutagenic Growth (MM2)
    Lightning Greaves (CMD)
    Umezawa's Jitte (V16)
    Paradox Engine (AER)
    Yawgmoth's Will (UZ)
    Phyrexian Obliterator (A25)
    Twilight Prophet (RIX)
    
    Out:
    Swiftfoot Boots
    Fleetwheel Cruiser
    Primal Amulet
    Crux of Fate
    Puppeteer Clique
    Disciple of Bolas
    
    Increased the overall card count by one (a mutagenic growth that I am
    calling colorless).  Also directed the doc integration to the Root
    Draft Template

--- js/Jason_gDocIntegrated.js
+++ js/Jason_gDocIntegrated.js
@@ -1,9 +1,9 @@
 function makeApiCall() {
   var params = {
     // The ID of the spreadsheet to retrieve data from.
-    spreadsheetId: '1ZelcAm369dDMPpnG20au7uL9zRWeJxRAulvjU0IlybM',  // TODO: Update placeholder value.
+    spreadsheetId: '1AW1oQzMEvve3lFhauXKNYxH51zNOFs5NE0DcuIAQ3k0',  // TODO: Update placeholder value.
     // The A1 notation of the values to retrieve.
-    range: 'B4:M43',  // TODO: Update placeholder value.
+    range: 'B4:O43',  // TODO: Update placeholder value.
     // How values should be represented in the output.
     // The default render option is ValueRenderOption.FORMATTED_VALUE.
     // valueRenderOption: '',  // TODO: Update placeholder value.
